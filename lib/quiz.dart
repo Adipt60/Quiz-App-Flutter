86f8067400fe9af30cef70fd5963197ff9fbb7a6
Aditya Patil <adipt1660@gmail.com>
2023-05-09 12:32:44 +0530

    Initialized Results Screen

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:quiz_app/questions_screen.dart';
+import 'package:quiz_app/results_screen.dart';
 import 'package:quiz_app/start_screen.dart';
 import 'package:quiz_app/data/quetions.dart';
 
@@ -20,7 +21,7 @@ class _QuizState extends State<Quiz> {
     if (selectedAnswers.length == questions.length) {
       setState(() {
         selectedAnswers = [];
-        activeScreen = StartScreen(switchScreen);
+        activeScreen = ResultsScreen(chosenAnswers: selectedAnswers);
       });
     }
   }
